b4acfe3d01cda53dd109a0f93da0fdb1b55a0f21
SoxKeepYouWarm <davidtow@buffalo.edu>
2015-03-27 12:20:43 -0400

    small changes

--- lab6.s
+++ lab6.s
@@ -19,8 +19,13 @@ score		= "score : ",10
 	ALIGN	
 the_board 	= "|---------------|\n|               |\n|               |\n|               |\n|               |\n|               |\n|               |\n|               |\n|       *       |\n|               |\n|               |\n|               |\n|               |\n|               |\n|               |\n|               |\n|---------------|"
 	ALIGN	
-current_direction = "1"	; 1 up, 2 left, 3 right, 4 down
+current_direction 		= 1		; 1 up, 2 left, 3 right, 4 down
 	ALIGN
+current_speed			= 1		; intial speed is 1
+	ALIGN
+termination_condition 	= 0 	; set to 1 when game should end
+	ALIGN
+
 
 lab6
 	stmfd sp!, {r4 - r12, lr}
@@ -28,20 +33,26 @@ lab6
 	bl uart_init	
 	bl interrupt_init
 	
+	ldr r4, =prompt
+	bl output_string
 	
-;loop
-;	ldr r4, =the_board
-;	bl output_string
-;	bl read_string
-;	ldr r4, =store_string
-;	bl output_string
-;	ldr r4, =newline
-;	bl output_string
-;	;mov r0, #0
-;	;bl write_character
+	ldr r4, =score
+	bl output_string
 	
-;	b loop	
+	ldr r4, =the_board
+	bl output_string
+	
+game_loop
+	
+	ldr r4, =termination_condition
+	ldr r5, [r4]
+	cmp r5, #1
+	bne game_loop
 
+game_termination
+	
+	;disable interrupts
+	
 	ldmfd sp!, {r4 - r12, lr}
 	bx lr
 	
@@ -58,10 +69,20 @@ interrupt_init
 	
 	ldr r4, =0xFFFFF00C 	; intterupt select register (VICIntSelect)
 	ldr r5, [r4]
-	orr r5, r5, #0x10		;enable bit 4 for timer 0
-	;orr r5, r5, #0x20		;enable bit 5 for timer 1
+	orr r5, r5, #0x10		;enable bit 4 for timer 0 FIQ
+	;orr r5, r5, #0x20		;enable bit 5 for timer 1 FIQ
 	orr r5, r5, #0x40		;enable bit 6 for fast interrupt
 	str r5, [r4]			
+
+	ldr r4, =0xE0004014		;timer 0
+	ldr r5, [r4]			;enable bit 3 to generate interrupt on mr1 == tc 
+	orr r5, r5, #0x18		;enable bit 4 to reset tc when mr1 == tc
+	str r5, [r4]					
+	
+	;ldr r4, =0xE0008014	;timer 1
+	;ldr r5, [r4]			;enable bit 3 to generate interrupt on mr1 == tc 
+	;orr r5, r5, #0x18		;enable bit 4 to reset tc when mr1 == tc
+	;str r5, [r4]
 
 	ldr r4, =0xE000C004		;enable uart interrupt read_data_available
 	ldr r5, [r4]
@@ -73,17 +94,6 @@ interrupt_init
 	ORR r0, r0, #0x80
 	MSR CPSR_c, r0
 	
-	ldr r4, =0xE0004014
-	ldr r5, [r4]			;enable bit 3 to generate interrupt on mr == tc 
-	orr r5, r5, #0x18		;enable bit 4 to reset tc when mr == tc
-	str r5, [r4]					
-	
-	ldr r4, =0xE0008014
-	ldr r5, [r4]			;enable bit 3 to generate interrupt on mr == tc 
-	orr r5, r5, #0x18		;enable bit 4 to reset tc when mr == tc
-	str r5, [r4]
-	
-
 
 	ldmfd sp!, {r4 - r12, lr}
 	bx lr
@@ -91,24 +101,122 @@ interrupt_init
 		
 		
 FIQ_Handler		
-	ldmfd sp!, {r0 - r1, lr}	
+	ldmfd sp!, {r0 - r2, lr}	
 read_data_interrupt
 	LDR r0, =0xE000C008
 	LDR r1, [r0]
 	and r1, r1, #1	;interrupt identification
 	cmp r1, #1		;set to 1 if no pending interrupts
 	beq FIQ_Exit
-				
+	
+	;read data interrupt handler code
 	bl read_data_handler
+	
+timer_one_interrupt
+	ldr r0, =0xE0004000
+	ldr r1, [r0]
+	and r2, r1, #2
+	cmp r2, #2		;is mr1 set
+	bne FIQ_Exit
+	
+	;timer 1 matches mr1 handler code
+	bl timer_one_mr_one_handler
 
 FIQ_Exit
-	LDMFD SP!, {r0 - r1, lr}
+	LDMFD SP!, {r0 - r2, lr}
 	SUBS pc, lr, #4
 		
 		
 		
+timer_one_mr_one_handler
+	stmfd sp!, {r0, r4, lr}
+			
+	bl game_mechanics
+	
+	ldr r0, =termination_condition
+	ldr r1, [r0]
+	cmp r1, #1
+	beq early_termination_break
+	
+	bl board_draw
+
+early_termination_break
+	ldmfd sp!, {r0, r4, lr}
+	bx lr	
+		
+
+game_mechanics
+	stmfd sp!, {r0 - r4, lr}
+	
+	bl calc_movement
+	bl update_score
+
+	ldmfd sp!, {r0 - r4, lr}
+	bx lr
+	
+	
+calc_movement
+	stmfd sp!, {r0 - r1, lr}
+	
+	;if destination is 45 or 124
+	;set r0 to 1 for early termination
+	
+	ldr r0, =current_direction
+	ldr r1, [r0]
+	
+	cmp r1, #1
+	beq move_up
+	
+	cmp r1, #2
+	beq move_left
+	
+	cmp r1, #3
+	beq move_right
+	
+	cmp r1, #4
+	beq move_down
+	
+	
+move_up
+	 
+	 ; move up algorithm
+	 
+	 b move_calc_done
+move_left
+
+	;move left algorithm
+
+	b move_calc_done
+move_right
+
+	;move right algorithm
+
+	b move_calc_done
+move_down
+	 
+	 ;move down algorithm
+	
+move_calc_done
+	ldmfd sp!, {r0 - r1, lr}
+	bx lr
+	
+update_score
+	stmfd sp!, {r0 - r4, lr}
+	
+	
+	ldmfd sp!, {r0 - r4, lr}
+	bx lr
+	
+	
+board_draw
+	stmfd sp!, {r0 - r4, lr}
+	
+	
+	ldmfd sp!, {r0 - r4, lr}
+	bx lr
+		
 read_data_handler
-	STMFD SP!, {r0, r4, lr}
+	stmfd sp!, {r0, r4, lr}
 	
 	BL read_character
 
@@ -133,7 +241,7 @@ set_direction_up
 	stmfd sp!, {r0 - r3}
 	
 	ldr r0, =current_direction
-	mov r1, #49
+	mov r1, #1
 	strb r1, [r0]
 	
 	ldmfd sp!, {r0 - r3}
@@ -143,7 +251,7 @@ set_direction_left
 	stmfd sp!, {r0 - r3}
 	
 	ldr r0, =current_direction
-	mov r1, #50
+	mov r1, #2
 	strb r1, [r0]
 
 	ldmfd sp!, {r0 - r3}
@@ -153,7 +261,7 @@ set_direction_right
 	stmfd sp!, {r0 - r3}
 	
 	ldr r0, =current_direction
-	mov r1, #51
+	mov r1, #3
 	strb r1, [r0]
 
 	ldmfd sp!, {r0 - r3}
@@ -163,15 +271,14 @@ set_direction_down
 	stmfd sp!, {r0 - r3}
 	
 	ldr r0, =current_direction
-	mov r1, #52
+	mov r1, #4
 	strb r1, [r0]
 
 	ldmfd sp!, {r0 - r3}
 	b read_data_handler_exit
 	
 read_data_handler_exit
-    LDMFD SP!, {r0, r4, lr}
-    BX LR
-	
+    ldmfd sp!, {r0, r4, lr}
+    bx lr
 		
 	end
--- library.s
+++ library.s
@@ -374,6 +374,66 @@ display_digit
 	LDMFD SP!, {r1 - r4, lr}
 	BX LR
 	
+; pass dividend and divisor into r0, and r1	
+div_and_mod
+	STMFD sp!, {r2-r7, lr}
+main
+	bl handle_sign
+	mov r2, #0		;initialize quotient to 0
+	mov r3, r0		;initialize remainder to dividend
+	mov r4, #16		;initialize counter to 16
+	lsl r1, #16		;logical shift left divisor 16 places
+	add r4, r4, #1	;offset addition for loop subtraction
+	
+main_loop
+	sub r4, r4, #1	
+	sub r3, r3, r1	;remainder = remainder - divisor
+	cmp r3, #0		; is remainder < 0?
+	blt remainder_less_than_zero
+	lsl r2, #1 		;left shift quotient
+	add r2, r2, #1	;lsb should be 1
+	b remainder_less_than_zero_branch_merged
+	
+remainder_less_than_zero
+	add r3, r3, r1	;remainder = remainder + divisor
+	lsl r2, #1		;left shift quotient, lsb = 0
+	
+remainder_less_than_zero_branch_merged
+	lsr r1, #1		;shift right divisor
+	cmp r4, #0		;is counter > 0?
+	bgt main_loop
+	;quotient and remainder are now known, time to check the sign flag
+	cmp r7, #0
+	beq flag_not_set
+	neg r2, r2		;negate quotient
+	neg r3, r3		;negate remainder
+flag_not_set
+	b end_program
+		
+handle_sign		
+	mov r7, #0		;initialize sign flag to 0
+	cmp r0, #0		;check dividend sign
+	bgt check_divisor
+	mov r7, #1		;increment sign flag if negative
+	neg r0, r0		;negate dividend to positive
+check_divisor
+	cmp r1, #0		;check divisor sign
+	bgt handle_sign_finished
+	neg r1, r1		;negate divisor to positive
+	cmp r7, #0		;check if sign flag already set
+	bgt sign_reset
+	mov r7, #1		;sign set
+	b handle_sign_finished
+sign_reset
+	mov r7, #0		;sign reset
+handle_sign_finished
+	bx lr	
+end_program
+	mov r0, r2		;move the quotient to return register
+	mov r1, r3		;move the remainder to return register
+	ldmfd sp!, {r2 - r7, lr}
+	bx lr	  		; Return to the C program
+
 
 pin_connect_block_setup_for_uart0
     STMFD sp!, {r0, r1, lr}
